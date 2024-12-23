a369add712b58485a92d58e59c5cbdec1d4cd9ff
shanuziya <shanuziyacp@gmail.com>
2024-12-24 01:48:50 +0530

    Fix authentication issue in Google OAuth

--- config/passport.js
+++ config/passport.js
@@ -20,7 +20,7 @@ passport.deserializeUser((id, done) => {
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: '/auth/google/callback',
+    callbackURL: 'bootsonline.xyz/auth/google/callback',
     scope: ['profile', 'email'],
 },
 async (accessToken, refreshToken, profile, done) => {
